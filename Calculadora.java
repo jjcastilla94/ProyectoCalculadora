cec46c401320bae3d3025d9c6ca0fe489d22d414
jjcastilla94 <alvarocastilla49@gmail.com>
2024-02-16 16:13:01 +0100

    Commit Inicial

---
+++ Calculadora.java
@@ -0,0 +1,19 @@
+public class Calculadora {
+    private int numero1;
+    private int numero2;
+
+    public Calculadora(int numero1, int numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+
+    public void realizarOperacion(String operacion) {
+        // Implementación de la operación
+    }
+
+    public int suma() {
+        return numero1 + numero2;
+    }
+
+    
+}
